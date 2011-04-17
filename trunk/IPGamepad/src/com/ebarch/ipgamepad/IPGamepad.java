8ffc6bc26962675e2d2f5e0a536dc1c4581db007
ericb@ericbarch.com <ericb@ericbarch.com@ccbca897-3457-ad36-bda8-1606147329f6>
2011-04-17 01:56:42 +0000

    Added the ControllerComponent class and finished UI redesign.

--- trunk/IPGamepad/src/com/ebarch/ipgamepad/IPGamepad.java
+++ trunk/IPGamepad/src/com/ebarch/ipgamepad/IPGamepad.java
@@ -2,6 +2,7 @@ package com.ebarch.ipgamepad;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -12,21 +13,24 @@ import android.widget.FrameLayout;
 
 
 public class IPGamepad extends Activity {
-	private float mPos1X, mPos2X;
-    private float mPos1Y, mPos2Y;
     FrameLayout mainLayout;
-    
-    private float viewWidth, viewHeight, rOuter;
-    private float leftCenterX, leftCenterY, rightCenterX, rightCenterY;
     
     /* CONSTANTS - Used for tweaking the UI */
     private final double JOYSTICK_BACK_SCALE_FACTOR = 1.25;
-    private final double JOYSTICK_FRONT_SCALE_FACTOR = 2.1;
-    private final int FINGER_OFFSET = 100;
+    private final double JOYSTICK_FRONT_SCALE_FACTOR = 1.15;
+    private final float JOYSTICK_TRIM = 30;	// Keep the joystick within the target zones - minor tweak
     
     // We want to know if we should send data over or not
     private boolean controlsAlive = false;
     
+    ControllerComponent leftIndicator;
+    ControllerComponent rightIndicator;
+    StaticLayout mainStaticLayout;
+    
+    public float viewWidth, viewHeight;
+	public float leftCenterX, leftCenterY;
+	public float rightCenterX, rightCenterY;
+	public float viewOffset, rOuter;
     
     /** Called when the activity is first created. */
     @Override
@@ -37,13 +41,11 @@ public class IPGamepad extends Activity {
         
         mainLayout = (FrameLayout)findViewById(R.id.main_view);
         
-        mainLayout.addView(new drawStaticLayout(this));
-        
-        // Draw the initial points before a touch event occurs - this just places them smack in the center of the "target zones"
-        moveIndicatorsToCenter();
+        mainStaticLayout = new StaticLayout(this);
+        mainLayout.addView(mainStaticLayout);  
     }
     
-    
+
     /* This is our touch event code that is called whenever there is an ACTION_MOVE, ACTION_DOWN, or ACTION_UP event */
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
@@ -52,10 +54,10 @@ public class IPGamepad extends Activity {
 
         if (action == MotionEvent.ACTION_MOVE || action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_UP) {
         	// Get the x,y position of each pointer. Use FINGER_OFFSET to get a more accurate position
-        	mPos1X = ev.getX(0);
-        	mPos1Y = ev.getY(0) - FINGER_OFFSET;
-        	mPos2X = ev.getX(1);
-        	mPos2Y = ev.getY(1) - FINGER_OFFSET;
+        	float mPos1X = ev.getX(0);
+        	float mPos1Y = ev.getY(0) - viewOffset;
+        	float mPos2X = ev.getX(1);
+        	float mPos2Y = ev.getY(1) - viewOffset;
         	
         	if (ev.getPointerCount() != 2)	// We don't see 2 fingers on the screen - bail out
         		exception = true;
@@ -93,48 +95,76 @@ public class IPGamepad extends Activity {
         return true;
     }
     
+    
     public void moveIndicatorsToCenter() {
-    	//TODO
+    	rightIndicator.setInitial();
+    	leftIndicator.setInitial();
+    	leftIndicator.invalidate();
+    	rightIndicator.invalidate();
     }
     
     public void moveRightIndicator(float x, float y) {
-    	//TODO
+    	rightIndicator.setX(x);
+    	rightIndicator.setY(y);
+    	rightIndicator.invalidate();
     }
 
     public void moveLeftIndicator(float x, float y) {
-    	//TODO
+    	leftIndicator.setX(x);
+    	leftIndicator.setY(y);
+    	leftIndicator.invalidate();
     }
     
+    
     /* This class handles drawing the initial target zones on the screen */
-    public class drawStaticLayout extends View {
+    public class StaticLayout extends View {
     	Bitmap joystick_back;
-        
-        public drawStaticLayout(Context context) {
+    	Context mContext;
+    	float scaledJBack, joyTop, joyLeft1, joyLeft2;
+    	
+        public StaticLayout(Context context) {
             super(context);
+            mContext = context;
             
             joystick_back = BitmapFactory.decodeResource(getResources(), R.drawable.joy_back);
+        }
+        
+        protected void onSizeChanged(int xNew, int yNew, int xOld, int yOld){
+        	// This method is called the first time the view is drawn on the screen - sets up all the values we'll need
+            super.onSizeChanged(xNew, yNew, xOld, yOld);
+            
+            viewWidth = (float)xNew;
+	        viewHeight = (float)yNew;
+	        
+	        Display display = getWindowManager().getDefaultDisplay();
+	        viewOffset = display.getHeight() - viewHeight;
+	        
+	        leftCenterX = (viewWidth / 4);
+	        leftCenterY = (viewHeight / 2);
+	        rightCenterX = (viewWidth * (float).75);
+	        rightCenterY = (viewHeight / 2);
+	        
+	        scaledJBack = (viewHeight / (float)JOYSTICK_BACK_SCALE_FACTOR);
+	        joyTop = leftCenterY - (scaledJBack / 2);
+	        joyLeft1 = leftCenterX - (scaledJBack / 2);
+	        joyLeft2 = rightCenterX - (scaledJBack / 2);
+	        
+	        rOuter = (scaledJBack / 2) - JOYSTICK_TRIM;
+	        joystick_back = Bitmap.createScaledBitmap(joystick_back, (int)scaledJBack, (int)scaledJBack, true);
+	        
+	        leftIndicator = new ControllerComponent(mContext, leftCenterX, leftCenterY, (float)JOYSTICK_FRONT_SCALE_FACTOR, R.drawable.joy_front);
+	        rightIndicator = new ControllerComponent(mContext, rightCenterX, rightCenterY, (float)JOYSTICK_FRONT_SCALE_FACTOR, R.drawable.joy_front);
+	        
+	        mainLayout.addView(leftIndicator);
+	        mainLayout.addView(rightIndicator);
         }
         
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
             
-            viewWidth = (float)mainLayout.getWidth();
-            viewHeight = (float)mainLayout.getHeight();
-            leftCenterX = (float)(viewWidth * .25);
-            leftCenterY = (float)(viewHeight / 2);
-            rightCenterX = (float)(viewWidth * .75);
-            rightCenterY = (float)(viewHeight / 2);
-            int scaledJBack = (int)((float)viewHeight / JOYSTICK_BACK_SCALE_FACTOR);
-            rOuter = (float)scaledJBack / (float)4;
-            int joyTop = (int)leftCenterY - (int)((float)scaledJBack / 2);
-            int joyLeft1 = (int)leftCenterX - (int)((float)scaledJBack / 2);
-            int joyLeft2 = (int)rightCenterX - (int)((float)scaledJBack / 2);
-            
-            joystick_back = Bitmap.createScaledBitmap(joystick_back, scaledJBack, scaledJBack, true);
-            
-            canvas.drawBitmap(joystick_back, joyLeft1, joyTop, null);
-            canvas.drawBitmap(joystick_back, joyLeft2, joyTop, null);
+	        canvas.drawBitmap(joystick_back, joyLeft1, joyTop, null);
+	        canvas.drawBitmap(joystick_back, joyLeft2, joyTop, null);
         }
     }
 }
