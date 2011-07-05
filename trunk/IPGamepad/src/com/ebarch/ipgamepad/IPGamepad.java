b2791fb4632847b39765f469b1ca2241c661701b
ericb@ericbarch.com <ericb@ericbarch.com@ccbca897-3457-ad36-bda8-1606147329f6>
2011-07-05 08:59:05 +0000

    Major overhaul (this will be version 0.3.0). Removed old joystick code, brought in MobileAnarchy joystick widgets. These are much more responsive and handle finger tracking better. Right now they are solid colors, but we'll want to reskin them in the future. Also added in joystick sensitivity and an option to turn robot headlights on/off. This will eventually become a separate view with toggle switches. The headlight status is sent as the 5th byte in the data packet.

--- trunk/IPGamepad/src/com/ebarch/ipgamepad/IPGamepad.java
+++ trunk/IPGamepad/src/com/ebarch/ipgamepad/IPGamepad.java
@@ -5,38 +5,21 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import com.MobileAnarchy.Android.Widgets.Joystick.DualJoystickView;
+import com.MobileAnarchy.Android.Widgets.Joystick.JoystickMovedListener;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.view.View;
-import android.widget.FrameLayout;
 
 
 public class IPGamepad extends Activity {
-    private volatile FrameLayout mainLayout;
     
-    /* CONSTANTS - Used for tweaking the UI */
-    private final double JOYSTICK_BACK_SCALE_FACTOR = 1.25;
-    private final double JOYSTICK_FRONT_SCALE_FACTOR = 1.15;
-    private final float JOYSTICK_TRIM = 50;	// Keep the joystick within the target zones - minor tweak
-    
-    // We want to know if we should send data over or not
-    private boolean controlsAlive = false;
-    
-    private ControllerComponent leftIndicator;
-    private ControllerComponent rightIndicator;
-    private StaticLayout mainStaticLayout;
     private SharedPreferences preferences;
     
     private NetworkingThread networkThread;
@@ -44,11 +27,12 @@ public class IPGamepad extends Activity {
     private InetAddress ipAddress;
     private int packetRate;
     private int port;
-    
-    private float viewWidth, viewHeight;
-	private float leftCenterX, leftCenterY;
-	private float rightCenterX, rightCenterY;
-	private float viewOffset, rOuter;
+    private boolean headlights;
+    private boolean leftActive, rightActive = false;
+    private int leftX, leftY, rightX, rightY = 0;
+    private static double scalingFactor = 0.25;
+	
+	private DualJoystickView joystick;
 	
     /** Called when the activity is first created. */
     @Override
@@ -57,11 +41,8 @@ public class IPGamepad extends Activity {
 
         setContentView(R.layout.main);
         
-        mainLayout = (FrameLayout)findViewById(R.id.main_view);
-        
-        // Create and draw the main layout for the joysticks
-        mainStaticLayout = new StaticLayout(this);
-        mainLayout.addView(mainStaticLayout);
+        joystick = (DualJoystickView)findViewById(R.id.dualjoystickView);
+        joystick.setOnJostickMovedListener(_listenerLeft, _listenerRight);
         
         // Initialize preferences
 		preferences = PreferenceManager.getDefaultSharedPreferences(this);
@@ -76,7 +57,6 @@ public class IPGamepad extends Activity {
         }
     }
     
-    
     /* Call this whenever the network settings need to be reloaded */
     public void updateNetworking() {
     	try {
@@ -88,149 +68,56 @@ public class IPGamepad extends Activity {
     	}
     }
     
-    
     @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu, menu);
 		return true;
 	}
-
     
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		startActivity(new Intent(this, Preferences.class));
 		return true;
 	}
-    
+	
+	private JoystickMovedListener _listenerLeft = new JoystickMovedListener() {
 
-    /* This is our touch event code that is called whenever there is an ACTION_MOVE, ACTION_DOWN, or ACTION_UP event */
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        final int action = ev.getAction();
-        boolean exception = false;	// If this becomes true at any point, don't update the indicator
+		@Override
+		public void OnMoved(int pan, int tilt) {
+			leftX = pan;
+			leftY = tilt;
+			leftActive = true;
+		}
 
-        if (action == MotionEvent.ACTION_MOVE || action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_UP) {
-        	// Get the x,y position of each pointer. Use FINGER_OFFSET to get a more accurate position
-        	float mPos1X = ev.getX(0);
-        	float mPos1Y = ev.getY(0) - viewOffset;
-        	float mPos2X = ev.getX(1);
-        	float mPos2Y = ev.getY(1) - viewOffset;
-        	
-        	if (ev.getPointerCount() != 2)	// We don't see 2 fingers on the screen - bail out
-        		exception = true;
-        	else if (((mPos1X < (viewWidth / 2)) && (mPos2X < (viewWidth / 2))) || ((mPos1X >= (viewWidth / 2)) && (mPos2X >= (viewWidth / 2))))
-        		exception = true;	// The user has placed 2 fingers in the same half of the screen - bail out
-        	
-        	if (!exception) {
-        		controlsAlive = true;	// Tell anyone else who needs to know that controls are valid and active
-	        	if (mPos1X < (viewWidth / 2)) {	// The first point is on the left
-	        		// Make sure the user's finger is within the target zone, otherwise don't update
-	        		if (Math.pow((mPos1X - leftCenterX),2) + Math.pow((mPos1Y - leftCenterY),2) < Math.pow(rOuter,2)) {
-	        			moveLeftIndicator(mPos1X,mPos1Y);
-	        		}
-	        		if (Math.pow((mPos2X - rightCenterX),2) + Math.pow((mPos2Y - rightCenterY),2) < Math.pow(rOuter,2)) {
-	        			moveRightIndicator(mPos2X,mPos2Y);
-	        		}
-	        	}
-	        	else {	// The first point is on the right
-	        		// Make sure the user's finger is within the target zone, otherwise don't update
-	        		if (Math.pow((mPos2X - leftCenterX),2) + Math.pow((mPos2Y - leftCenterY),2) < Math.pow(rOuter,2)) {
-	        			moveLeftIndicator(mPos2X,mPos2Y);
-	        		}
-	        		if (Math.pow((mPos1X - rightCenterX),2) + Math.pow((mPos1Y - rightCenterY),2) < Math.pow(rOuter,2)) {
-	        			moveRightIndicator(mPos1X,mPos1Y);
-	        		}
-	        	}
-        	}
-        	else {
-        		// We had an exception - move the indicators to the center and make controlsAlive false
-        		controlsAlive = false;
-        		moveIndicatorsToCenter();
-        	}
-        }
-        
-        return true;
-    }
-    
-    
-    public void moveIndicatorsToCenter() {
-    	// Move indicators to initial position and Redraw
-    	rightIndicator.setInitial();
-    	leftIndicator.setInitial();
-    }
-    
-    
-    public void moveRightIndicator(float x, float y) {
-    	// Move right indicator to x/y and Redraw
-    	rightIndicator.setXY(x, y);
-    }
+		@Override
+		public void OnReleased() {
+			leftActive = false;
+		}
+		
+		public void OnReturnedToCenter() {
+			leftActive = false;
+		};
+	};
+	
+    private JoystickMovedListener _listenerRight = new JoystickMovedListener() {
 
-    
-    public void moveLeftIndicator(float x, float y) {
-    	// Move left indicator to x/y and Redraw
-    	leftIndicator.setXY(x, y);
-    }
-    
-    
-    /* This class handles drawing the initial target zones and indicators on the screen */
-    public class StaticLayout extends View {
-    	Bitmap joystick_back;
-    	Context mContext;
-    	float scaledJBack, joyTop, joyLeft1, joyLeft2;
-    	
-        public StaticLayout(Context context) {
-            super(context);
-            mContext = context;
-            
-            joystick_back = BitmapFactory.decodeResource(getResources(), R.drawable.joy_back);
-        }
-        
-        // This method is called the first time the view is drawn on the screen - sets up all the values we'll need
-        protected void onSizeChanged(int xNew, int yNew, int xOld, int yOld){
-            super.onSizeChanged(xNew, yNew, xOld, yOld);
-            
-            viewWidth = (float)xNew;
-	        viewHeight = (float)yNew;
-	        
-	        // Get the height of the entire screen and subtract out the height of our view to find the offset for touch events
-	        Display display = getWindowManager().getDefaultDisplay();
-	        viewOffset = display.getHeight() - viewHeight;
-	        
-	        // Find the centers of the joysticks
-	        leftCenterX = (viewWidth / 4);
-	        leftCenterY = (viewHeight / 2);
-	        rightCenterX = (viewWidth * (float).75);
-	        rightCenterY = (viewHeight / 2);
-	        
-	        // get values for top/left when drawing joysticks
-	        scaledJBack = (viewHeight / (float)JOYSTICK_BACK_SCALE_FACTOR);
-	        joyTop = leftCenterY - (scaledJBack / 2);
-	        joyLeft1 = leftCenterX - (scaledJBack / 2);
-	        joyLeft2 = rightCenterX - (scaledJBack / 2);
-	        
-	        rOuter = (scaledJBack / 2) - JOYSTICK_TRIM;	// Radius of the target zones - tweaked by JOYSTICK_TRIM
-	        joystick_back = Bitmap.createScaledBitmap(joystick_back, (int)scaledJBack, (int)scaledJBack, true);
-	        
-	        // Create the joystick indicators
-	        leftIndicator = new ControllerComponent(mContext, leftCenterX, leftCenterY, rOuter, (float)JOYSTICK_FRONT_SCALE_FACTOR, R.drawable.joy_front);
-	        rightIndicator = new ControllerComponent(mContext, rightCenterX, rightCenterY, rOuter, (float)JOYSTICK_FRONT_SCALE_FACTOR, R.drawable.joy_front);
-	        
-	        // Initial draw of the joystick indicators
-	        mainLayout.addView(leftIndicator);
-	        mainLayout.addView(rightIndicator);
-        }
-        
-        @Override
-        protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            
-            // Draw the joystick backs
-	        canvas.drawBitmap(joystick_back, joyLeft1, joyTop, null);
-	        canvas.drawBitmap(joystick_back, joyLeft2, joyTop, null);
-        }
-    }
-    
+    	@Override
+		public void OnMoved(int pan, int tilt) {
+    		rightX = pan;
+			rightY = tilt;
+			rightActive = true;
+		}
+
+		@Override
+		public void OnReleased() {
+			rightActive = false;
+		}
+		
+		public void OnReturnedToCenter() {
+			rightActive = false;
+		};
+	};
     
     /* The main networking thread that sends the joystick UDP packets */
     class NetworkingThread extends Thread {
@@ -238,11 +125,18 @@ public class IPGamepad extends Activity {
 
         public void run() {
                 while (!stop) {
-                	if (controlsAlive) {
+                	if (leftActive || rightActive) {
                 		// Robot is enabled - let's send some data
     		    		try {
-    		    			// A packet contains 4 bytes - leftJoystickY, leftJoystickX, rightJoystickY, rightJoystickX
-    						byte[] buf = new byte[] { leftIndicator.getJoystickByteY(), leftIndicator.getJoystickByteX(), rightIndicator.getJoystickByteY(), rightIndicator.getJoystickByteX() };
+    		    			// A packet contains 5 bytes - leftJoystickY, leftJoystickX, rightJoystickY, rightJoystickX, headlights
+    		    			byte headlightByte;
+    		    			
+    		    			if (headlights)
+    		    				headlightByte = (byte) 255;
+    		    			else
+    		    				headlightByte = (byte) 0;
+    		    			
+    						byte[] buf = new byte[] { joystickSensitivity(leftY), joystickSensitivity(leftX), joystickSensitivity(rightY), joystickSensitivity(rightX), headlightByte };
     						DatagramPacket p = new DatagramPacket(buf, buf.length, ipAddress, port);
     						udpSocket.send(p);
     					} catch (Exception e) {}
@@ -265,7 +159,25 @@ public class IPGamepad extends Activity {
                 stop = true;
         }
     }
-
+    
+    private static byte joystickSensitivity(int input) {
+    	double doubleJoystick = mapValue((double)input, -150, 150, -1, 1);
+    	
+    	double scaledValue = ((Math.pow(doubleJoystick,3)) * scalingFactor) + ((1 - scalingFactor) * doubleJoystick);
+    	
+    	int result = (int)mapValue(scaledValue, -1, 1, 0, 255);
+    	
+    	if (result < 0)
+    		result = 0;
+    	else if (result > 255)
+    		result = 255;
+    	
+    	return (byte)result;
+    }
+    
+    public static double mapValue(double input, double inMin, double inMax, double outMin, double outMax) {
+    	return (input - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
     
     /* Call this to start the main networking thread */
     public synchronized void startNetworkingThread(){
@@ -274,7 +186,6 @@ public class IPGamepad extends Activity {
                 networkThread.start();
         }
     }
-
     
     /* Call this to stop the main networking thread */
     public synchronized void stopNetworkingThread(){
@@ -284,7 +195,6 @@ public class IPGamepad extends Activity {
         }
     }
     
-    
     @Override
     protected void onPause() {
     	// End Ethernet communications
@@ -293,13 +203,18 @@ public class IPGamepad extends Activity {
     	super.onPause();
     }
     
-    
     @Override
     protected void onResume() {
     	super.onResume();
     	
     	// Update networking settings
     	updateNetworking();
+    	
+    	// Update headlight status
+    	headlights = preferences.getBoolean("headlights", false);
+    	
+    	// Update joystick scaler
+    	scalingFactor = Double.parseDouble(preferences.getString("scalingFactor", "0.25"));
     	
     	// Begin Ethernet communications
     	startNetworkingThread();
